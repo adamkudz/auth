32cdb72f9329c857be843ece4ae0a2980fca2b59
Adam <adam.kudzin@gmail.com>
2019-09-28 13:26:29 -0400

    final commit before merge

--- routes/member.route.js
+++ routes/member.route.js
@@ -15,6 +15,43 @@ router.get("/", async (req, res) => {
     res.json({ message: err })
   }
 })
+// passport.use(
+//   new LocalStrategy(function(username, password, done) {
+//     User.findOne(
+//       { username: username, password: password, session: false },
+//       function(err, user) {
+//         if (err) {
+//           return done(err)
+//         }
+//         if (!user) {
+//           return done(null, false, { message: "Incorrect username." })
+//         }
+//         if (!user.validPassword(password)) {
+//           return done(null, false, { message: "Incorrect password." })
+//         }
+//         return done(null, user)
+//       },
+//     )
+//   }),
+// )
+// passport.serializeUser(function(user, done) {
+//   done(null, user.id)
+// })
+
+// passport.deserializeUser(function(id, done) {
+//   User.findById(id, function(err, user) {
+//     done(err, user)
+//   })
+// })
+
+// Router.post(
+//   "/login",
+//   passport.authenticate("local", {
+//     successRedirect: "/members",
+//     fialureRedirect: "/login",
+//     failureFlash: true,
+//   }),
+// )
 
 //Get One Member
 
